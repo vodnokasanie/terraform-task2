4ec3e358fb3e8c6ec02ebbad8fe10772a97b856a
Torgen <118271296+vodnokasanie@users.noreply.github.com>
2025-10-30 15:05:03 +0600

    M

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "cmtr-ghjc0xhd-ec2" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-0acf18f82e1faf274"
+  subnet_id              = "subnet-049c956da43eada5a"
   key_name               = aws_key_pair.cmtr-ghjc0xhd-keypair.key_name
   vpc_security_group_ids = ["sg-04949f18514fd9783"]
 
